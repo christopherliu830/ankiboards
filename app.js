4f41cae406609f6191c65c79699ae8415273bc91
Christopher Liu <christopherliu830@gmail.com>
2020-09-21 19:52:00 -0400

    frontend now uses firebase

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var secureRouter = require('./routes/users');
 var oauthRouter = require('./routes/oauth');
 var cors = require('cors');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const OAuth2Server = require('oauth2-server');
+const OAuth2Server = require('express-oauth-server');
 const {Request, Response} = require('oauth2-server');
 const OAuthModel = require('./model/oauth2model');
 
@@ -39,16 +39,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/auth', oauthRouter);
 app.use('/', indexRouter);
-app.use('/', passport.authenticate('jwt', {session: false}), usersRouter);
+app.use('/', oauthRouter);
+app.use('/token', app.oauth.token());
+app.use('/', passport.authenticate('jwt', {session: false}), secureRouter);
+app.get('/profile', passport.authenticate('jwt', {session: false}), (req, res) => res.send(200));
 app.use('/ass2', (req, res, next) => {
   console.log("hello");
   const request = new Request(req);
   const response = new Response(res);
   req.app.oauth.authenticate(request, response, () => {
   })
-  .then(token => next());
+  .then(next());
 });
 
 // catch 404 and forward to error handler
