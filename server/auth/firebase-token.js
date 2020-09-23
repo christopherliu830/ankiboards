08eb3778d08d130d86bd752b1c847b0400a5239d
Christopher Liu <christopherliu830@gmail.com>
2020-09-22 23:11:51 -0400

    accidently ignored token js file

---
+++ server/auth/firebase-token.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const bearerToken = require('express-bearer-token');
+const UserModel = require('../model/model').UserModel;
+
+module.exports = (req, res, next) => {
+  bearerToken()(req, res, () => {
+    const token = req.token;
+    if (!token) return res.status(500).send('You need to provide a token to access the server');
+    admin.auth().verifyIdToken(token)
+      .then(decodedToken => {
+        return UserModel.findOne({firebaseUid: decodedToken.uid});
+      })
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => {
+        res.status(404).send(err.toString());
+    });
+  })
+};
