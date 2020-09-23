6659fdf11e05f091b861d41434d61cc132380278
Christopher Liu <christopherliu830@gmail.com>
2020-09-22 23:11:00 -0400

    migrating to live server

--- app.js
+++ app.js
@@ -24,11 +24,20 @@ mongoose.connection.on('error', error => console.log(error) );
 mongoose.Promise = global.Promise;
 
 // view engine setup
+frontendCors = {
+  origin: /ankiboards.com/,
+  credentials: true,
+}
+
+apiCors = {
+  origin: true,
+  credentials: true
+}
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(cors({
-  origin: true,
+  origin: ["https://ankiboards.com", /localhost/],
   credentials: true,
 }));
 app.use(logger('dev'));
@@ -37,7 +46,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
-app.use('/', require('./auth/firebase-token'), secureRouter);
+app.use('/', cors(frontendCors), require('./auth/firebase-token'), secureRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
