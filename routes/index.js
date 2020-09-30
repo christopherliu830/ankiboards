2f0f8239a2d41377890add90469a6ce78d926977
Christopher Liu <christopherliu830@gmail.com>
2020-09-30 11:00:12 -0400

    sends data to server

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const admin = require('firebase-admin');
-const UserModel = require('../model/model').UserModel;
+const {UserModel, ReviewEntry }= require('../model/model');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -33,28 +33,6 @@ router.post('/signup', async (req, res, next) => {
   }
 });
 
-router.post('/login', async (req, res, next) => {
-  passport.authenticate('local', {session: false}, async (err, user, info) => {
-    try {
-      if (err || !user) {
-        return res.status(404).send('User Not Found');
-      }
-      req.login(user, { session : false }, async error => {
-        if (error) return next(error);
-        const body = { _id : user._id, email : user.email, username : user.username};
-        const token = jwt.sign({payload:body}, 'top_secret');
-        const [ header, payload, signature ] = token.split('.');
-        res.cookie('header.payload', `${header}.${payload}`, {sameSite: true, secure: true});
-        res.cookie('signature', signature, {sameSite: true, secure: true, httpOnly: true});
-        res.cookie('test', 'hi');
-        res.send({token: token});
-      });
-    } catch (error) {
-      return next(error);
-    }
-  })(req, res, next);
-});
-
 router.post('/search', (req, res, next) => {
   const { query } = req.body;
   const re = new RegExp('^' + query);
@@ -78,7 +56,8 @@ router.get('/user/:id', async (req, res, next) => {
       {username: req.params.id},
     ]
   });
-  res.status(200).send({ankiInfo: user.ankiInfo, username: user.username});
+  const reviews = await ReviewEntry.find({userid: user._id});
+  res.status(200).send({ankiInfo: {...user.ankiInfo, revlog: reviews}, username: user.username});
 });
 
 
