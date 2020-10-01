90df8c17f570e29bc9daa2f4a8d11c58bef5b1ba
Christopher Liu <christopherliu830@gmail.com>
2020-09-30 20:21:40 -0400

    basic config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,9 @@
-import React, {useState} from 'react';
-import {Route, Switch, useLocation } from 'react-router-dom';
-import * as pages from './pages';
-import Navbar from './components/Navbar';
-import Cookies from 'js-cookie';
-import './App.css';
-
+import React from 'react';
 function App() {
-  const [userId, setUserId] = useState(null);
-  const location = useLocation();
-
-  const handleSignOut = e => {
-    e.preventDefault();
-    if (Cookies.get('header.payload')) Cookies.remove('header.payload');
-    setUserId(null);
-  }
 
   return (
-    <div className="d-flex flex-column h-100 align-content-stretch">
-      <Navbar userId={userId} onLogout={() => setUserId(null)}/>
-      <Switch>
-        <Route path='/' exact><pages.HomePage/></Route>
-        <Route path='/login'><pages.LoginPage/></Route>
-        <Route path='/signup'><pages.SignUpPage/></Route>
-        <Route path='/profile'><pages.ProfileSettingsPage/></Route> 
-        <Route path='/about'><pages.AboutPage/></Route>
-        {/* <Route path='/get-started'><pages.GetStartedPage/></Route> */}
-        <Route path='/oauth'><pages.OAuthPage/></Route>
-        <Route path='/user/:id'><pages.UserPage/></Route>
-      </Switch>
+    <div>
+      penis
     </div>
   )
 }
